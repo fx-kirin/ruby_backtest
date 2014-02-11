5c83441dcc7eb9839ecb87f00f4c916f0b719d65
pgkireek <ono.kirin@gmail.com>
2014-02-11 12:06:11 +0900

    Added the policy.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ruby-backtest
 =============
 
 The Backtesting program with Ruby.
+
+There are some choises for python but not for Ruby.
+So I decided to make Ruby backtesting programm.
+If you feel any interest, let's make it better together.
---
+++ test/test_price_feeds.rb
@@ -0,0 +1,13 @@
+# Test PriceFeeds
+require 'test/unit'
+
+# PriceFeeds class has these methods, close, open, high and low to get market data.
+class PriceFeeds < Test::Unit::TestCase
+  def set_data
+    
+  end
+  
+  def test_close
+    
+  end
+end
